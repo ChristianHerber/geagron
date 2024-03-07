20e454322db10f01fe9b217afa60ba8cf8c99773
Christian Herber <christianfaldox@gmail.com>
2024-03-07 15:44:00 -0400

    add pasta commons com messages para exibir mensagens em todas as telas

---
+++ app/pages/commons/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+    @$msg = $_GET['msg'];
+    @$alertBg = $_GET['alertBg'];
+
+    $css = 'position:absolute;top:85px;right:2%;';
+
+    if (!empty($msg)):
+
+        echo "
+            <div 
+                class='alert alert-$alertBg border-$alertBg alert-dismissible fade show'
+                style='{$css}'
+                role='alert' 
+                id='alert-msg'
+            >
+                <i class='fa fa-info-circle'></i>
+                $msg
+            </div>
+        ";
+
+    endif;
